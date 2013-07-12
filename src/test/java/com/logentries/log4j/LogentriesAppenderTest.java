723bdd97f1919e6af5a75ec50dfbd77d0f9740ff
StephenHynes7 <stephen@logentries.com>
2013-07-12 14:26:26 +0100

    Added more unit tests

--- src/test/java/com/logentries/log4j/LogentriesAppenderTest.java
+++ src/test/java/com/logentries/log4j/LogentriesAppenderTest.java
@@ -1,15 +1,14 @@
-package com.logentries.logback;
+package com.logentries.log4j;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 import org.junit.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+
+import com.logentries.log4j.LogentriesAppender;
+
+
 
 public class LogentriesAppenderTest {
-	
-	private static final Logger log = LoggerFactory
-			.getLogger(LogentriesAppenderTest.class);
 	
 	private static final String token = "some-token";
 	private static final String location = "some location";
@@ -17,19 +16,18 @@ public class LogentriesAppenderTest {
 	private static final String facility = "DAEMON";
 	
 	@Test
-	public void setterTests() {
+	public void settersTest() {
 		LogentriesAppender le = new LogentriesAppender();
 		le.setHttpPut(true);
 		le.setToken(token);
 		le.setLocation(location);
 		le.setKey(accountKey);
 		le.setSsl(true);
-		le.setFacility(facility);
 		assertEquals(le.le_async.getToken(),token);
 		assertEquals(le.le_async.getHttpPut(),true);
 		assertEquals(le.le_async.getKey(),accountKey);
 		assertEquals(le.le_async.getLocation(),location);
-		assertEquals(le.le_async.getSsl(),true);		 
-	}		
-}
+		assertEquals(le.le_async.getSsl(),true);		
+	}
 
+}
