1dfff583ef92527f235fdfa96e4456b076195253
Björn Raupach <raupach.bjoern@googlemail.com>
2014-08-31 18:41:14 +0200

    Removed license header and made the call for a line separator 1.6 compatibel

--- src/main/java/com/logentries/jul/LogentriesHandler.java
+++ src/main/java/com/logentries/jul/LogentriesHandler.java
@@ -1,18 +1,3 @@
-/*
- * Copyright 2014 Björn Raupach.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package com.logentries.jul;
 
 import java.io.IOException;
@@ -114,7 +99,7 @@ public final class LogentriesHandler extends Handler {
         try {
             msg = getFormatter().format(record);
             // replace line separators with unicode equivalent
-            msg = msg.replace(System.lineSeparator(), "\u2028");
+            msg = msg.replace(System.getProperty("line.separator"), "\u2028");
         } catch (Exception e) {
             reportError("Error while formatting.", e, FORMAT_FAILURE);
         }
