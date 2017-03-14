38f6b13ad3bf47bba8a8d4bff247c32d6b544763
stopal <stopal@rapid7.com>
2017-03-14 14:37:12 +0000

    LOG-8798 fixed catching IOException logic in opening tcp connection

--- src/main/java/com/logentries/net/LogentriesClient.java
+++ src/main/java/com/logentries/net/LogentriesClient.java
@@ -1,6 +1,7 @@
 package com.logentries.net;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.Socket;
 import java.net.UnknownHostException;
@@ -30,12 +31,12 @@ public class LogentriesClient
 	/** Port number for SSL HTTP PUT/TLS Token TCP logging on Logentries server. */
 	private static final int LE_SSL_PORT = 443;
 
-	final SSLSocketFactory ssl_factory;
+	private final SSLSocketFactory ssl_factory;
 	private boolean ssl_choice = false;
 	private boolean http_choice = false;
 	private Socket socket;
-	private OutputStream stream;
-	private String dataHubServer = LE_TOKEN_API;
+    private OutputStream outputStream;
+    private String dataHubServer = LE_TOKEN_API;
 	private int dataHubPort = LE_PORT;
 	private boolean useDataHub = false;
 	
@@ -44,7 +45,7 @@ public class LogentriesClient
 		if(isUsingDataHub){
 			ssl_factory = null; // DataHub does not support input over SSL for now,
 			ssl_choice = false; // so SSL flag is ignored
-			useDataHub = isUsingDataHub;
+			useDataHub = true;
 			dataHubServer = server;
 			dataHubPort = port;
 		}
@@ -77,32 +78,33 @@ public class LogentriesClient
 		}
 	}
 	
-	public void connect() throws UnknownHostException, IOException
+	public void connect() throws IOException
 	{
 		if(ssl_choice) {
 			if(http_choice)
 			{
 				SSLSocket s = (SSLSocket) ssl_factory.createSocket( getAddress(), getPort() );
-				s.setTcpNoDelay( true);
 				s.startHandshake();
 				socket = s;
 			}else{
-				socket = SSLSocketFactory.getDefault().createSocket( getAddress(), getPort() );
+				socket = ssl_factory.createSocket(getAddress(), getPort());
 			}
 		}else{
 			socket = new Socket( getAddress(), getPort() );
 		}
 
-		this.stream = socket.getOutputStream();
-	}
+		this.socket.setKeepAlive(true);
+		this.outputStream = socket.getOutputStream();
+    }
 	
 	public void write(byte[] buffer, int offset, int length) throws IOException
 	{
-		if(this.stream == null){
+		if(this.outputStream == null) {
 			throw new IOException();
 		}
-		this.stream.write(buffer, offset, length);
-		this.stream.flush();
+
+		this.outputStream.write(buffer, offset, length);
+		this.outputStream.flush();
 	}
 	
 	public void close()
